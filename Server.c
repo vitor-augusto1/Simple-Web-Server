314f2f6ecc7e84cc6938a2105c82d7e78a4c3cdb
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-05 08:06:04 -0300

    feat: Create a new function to construct a new server

--- Server.c
+++ Server.c
@@ -2,3 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Server server_constructor(int domain, int protocol, int service,
+    u_long interface, int port, int backlog,
+    void (*launch)(struct Server *server))
+{
+}
