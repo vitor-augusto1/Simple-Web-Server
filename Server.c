4a1be0a3b4bfeb5132896a4bf5a96e5e76498344
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-05 08:07:37 -0300

    feat: Add server data to the server's struct

--- Server.c
+++ Server.c
@@ -6,4 +6,18 @@ struct Server server_constructor(int domain, int protocol, int service,
     u_long interface, int port, int backlog,
     void (*launch)(struct Server *server))
 {
+  struct Server server;
+
+  server.domain = domain;
+  server.service = service;
+  server.protocol = protocol;
+  server.interface = interface;
+  server.port = port;
+  server.backlog = backlog;
+
+  server.address.sin_family = domain;
+  server.address.sin_port = htons(port);
+  server.address.sin_addr.s_addr = htonl(interface);
+
+  server.socket = socket(domain, service, protocol);
 }
