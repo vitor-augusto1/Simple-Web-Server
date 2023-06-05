4e9f315ed92a5fa14b32fa8e7eb1a9eef93a5e2b
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-05 08:04:08 -0300

    feat: Create new struct to the server

--- Server.h
+++ Server.h
@@ -4,6 +4,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+struct Server {
+  int domain;
+  int protocol;
+  int service;
+  u_long interface;
+  int port;
+  int backlog;
+  struct sockaddr_in address;
+  int socket;
+  void (*launch)(struct Server *server);
+};
 
 
 #endif /* Server_h */
