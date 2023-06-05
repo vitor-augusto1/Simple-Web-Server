8ef992df269555b228b24afa9bf0bb8fbf2bd541
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-05 08:10:32 -0300

    feat: Define the hello, world message

--- test.c
+++ test.c
@@ -7,6 +7,10 @@
 void launch(struct Server *server)
 {
   char buffer[30000];
+  char *hello = "HTTP/1.1 200 OK\nDate: Mon, 05 Jun 2023 06:25:50 \
+                 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 jul \
+                 2009 19:15:56 GMT\nContent-length: 88\nContent-Type: text/html \
+                 \nConnection: Closed\n\n<html><body><h1>Hello, World!</h1><body></html>";
 
 
 }
