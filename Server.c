37bd1c3d80ddb6ceb377445ac95e6dc3cc42f43e
vitor-augusto1 <v1t0r4ug@gmail.com>
2023-06-05 08:08:02 -0300

    feat: Exit with code 1 if any error occurs

--- Server.c
+++ Server.c
@@ -20,4 +20,20 @@ struct Server server_constructor(int domain, int protocol, int service,
   server.address.sin_addr.s_addr = htonl(interface);
 
   server.socket = socket(domain, service, protocol);
+  if (server.socket == 0) {
+    perror("Failed to connect socket...\n");
+    exit(1);
+  }
+
+  if ((bind(server.socket, (struct sockaddr *)&server.address, 
+      sizeof(server.address))) < 0) {
+    perror("Failed to bind socket...\n");
+    exit(1);
+  }
+
+  if ((listen(server.socket, server.backlog)) < 0) {
+    perror("Failed to start listening...\n");
+    exit(1);
+  }
+
 }
